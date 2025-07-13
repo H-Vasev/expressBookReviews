1768386ea87a926bdbb4cc80adb33a8bcca6f955
H.Vasev <hristovasev999@gmail.com>
2025-07-13 13:41:44 +0100

    Implementing adding reviews and deleting reviews

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,59 +1,108 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{
-  return typeof username === "string" && username.length > 3
-}
+const isValid = (username) => {
+  return typeof username === "string" && username.length > 3;
+};
 
-const authenticatedUser = (username,password)=>{ 
-  const user = users.find((u) => u.username === username && u.password === password);
+const authenticatedUser = (username, password) => {
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
 
-  if(user){
+  if (user) {
     return true;
-  }else {
-    return false
+  } else {
+    return false;
   }
-}
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {  
-  const {username, password} = req.body
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-   if (!username || !password) {
+  if (!username || !password) {
     return res
       .status(400)
       .json({ message: "Username and password are not provided!" });
   }
 
-  const currentUser = users.find((u) => u.username === username)
+  const currentUser = users.find((u) => u.username === username);
 
-  if(!currentUser){
-    return res.status(400).send({message: `User with provided username: ${username}, does not exist!`})
+  if (!currentUser) {
+    return res
+      .status(400)
+      .send({
+        message: `User with provided username: ${username}, does not exist!`,
+      });
   }
 
-  if(!authenticatedUser(username, password)){
-    return res.status(400).send({message: "Invalid credentials!"})
+  if (!authenticatedUser(username, password)) {
+    return res.status(400).send({ message: "Invalid credentials!" });
   }
 
-  const accessToken = jwt.sign({username}, "secret_key", {expiresIn: "1h"});
+  const accessToken = jwt.sign({ username }, "secret_key", { expiresIn: "1h" });
 
   req.session.authorization = {
     accessToken,
-    username
-  }
+    username,
+  };
 
-  return res.status(200).send({message: "Login successful.", token: accessToken})
+  return res
+    .status(200)
+    .send({ message: "Login successful.", token: accessToken });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookForReview = books[req.params.isbn];
+
+  if (!bookForReview) {
+    return res
+      .status(400)
+      .send({ message: "The provided book does not exist!" });
+  }
+
+  const currUser = req.session.authorization?.username
+
+  if(!currUser){
+    return res.status(400).send({message: "Must be logged in to submit a review!"})
+  }
+
+  const review = req.body.review;
+   if(!review){
+    return res.status(400).send({message: "The review message is a must!"})
+  }
+  
+  bookForReview.reviews[currUser] = review
+
+  return res.status(200).send({message:"The review is added successfully", bookForReview})
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const book = books[req.params.isbn]
+
+  if(!book){
+    return res.status(400).send({message: "The book does not exist!"})
+  }
+
+  const currUser = req.session.authorization?.username
+  if(!currUser){
+    return res.status(400).send({message: "You must login to delete a review!"})
+  }
+
+  if(!book.reviews[currUser]){
+    return res.status(400).send({message: "There is no review form this user!"})
+  }
+
+  delete book.reviews[currUser]
+
+  return res.status(200).send({message: `Review by: ${currUser}, was successfully deleted!`, book})
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
